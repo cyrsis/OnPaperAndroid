0355b14dfd2a77535c859de17d5c664e5576f150
Victor <em3888@gmail.com>
2016-12-30 18:17:42 +0800

    Get the Outh Working in here

--- OnPaper/app/src/main/java/tech/getonpaper/victor/onpaper/MainActivity.java
+++ OnPaper/app/src/main/java/tech/getonpaper/victor/onpaper/MainActivity.java
@@ -1,13 +1,68 @@
 package tech.getonpaper.victor.onpaper;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String PATH_TOS = "";
+    private Button loginButton;
+    private FirebaseAuth auth;
+    private static final int RC_SIGN_IN = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
+        if(isUserLogin()){loginUser();}
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        loginButton = (Button)findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTosUrl(PATH_TOS)
+                        .build(), RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                loginUser();
+            }
+            if(resultCode == RESULT_CANCELED){
+                displayMessage(getString(R.string.signin_failed));
+            }
+            return;
+        }
+        displayMessage(getString(R.string.unknown_response));
+    }
+    private boolean isUserLogin(){
+        if(auth.getCurrentUser() != null){
+            return true;
+        }
+        return false;
+    }
+    private void loginUser(){
+        Intent loginIntent = new Intent(MainActivity.this, SigninActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+    private void displayMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
---
+++ OnPaper/app/src/main/java/tech/getonpaper/victor/onpaper/SigninActivity.java
@@ -0,0 +1,13 @@
+package tech.getonpaper.victor.onpaper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
